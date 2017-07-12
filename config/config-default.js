bdda9888c7db699e203092aef00b271eac68f3e3
kdudziak <kdudziak@fkbl.com>
2017-07-11 21:59:43 -0400

    deploy ready

--- config/config-default.js
+++
@@ -1,7 +0,0 @@
-config = module.exports = {};
-
-config.aws = {
-  AWS_ACCESS_KEY:'0QH2xxxxxxxxxxxxxxxx',
-  AWS_SECRET_KEY:'iIeexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-  S3_BUCKET: 'bucketname'
-};
